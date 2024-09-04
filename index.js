1a68df703922a43fa4a65bff3257c5efdeb29d4a
Michael Sullivan <sullivanmichael344@gmail.com>
2024-09-04 19:19:03 -0400

    Fix merge function

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function mergeSort(arr){
 
     if (arr.length === 1) return arr
 
-   // let arr = [2,1,4,3]
+   
 
     let leftHalf = mergeSort(arr.slice(0, Math.floor((arr.length)/2)))
     let rightHalf = mergeSort(arr.slice(Math.floor((arr.length)/2)))
@@ -17,16 +17,15 @@ function mergeSort(arr){
 
 function merge(left, right){
     let arr = []
-    let count = 0;
-    while(count < left.length || count < right.length){
+    while(left.length > 0 && right.length > 0){
     if (left[0] < right[0]){
         let item = left.shift()
         arr = arr.concat(item)
-        count++
+        
     } else{
         let item = right.shift()
         arr = arr.concat(item)
-        count++
+        
     }
 }
     let remaining = left.length > 0 ? left : right
@@ -41,4 +40,4 @@ function merge(left, right){
 
 
 
-console.log(mergeSort([2,1,4,3]))
+console.log(mergeSort([2,1,4,3,9]))
